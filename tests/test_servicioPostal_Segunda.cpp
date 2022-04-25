c3ddaca632c02e707fd41fcc836d6c921513e433
JuanAguilarTorres <Juan.aguilartorres@ucr.ac.cr>
2022-04-24 22:27:07 -0600

    Añadidas Unit Tests de Servicio Postal, segunda C.

---
+++ tests/test_servicioPostal_Segunda.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include "../src/envioServicioPostal.h"
+
+namespace
+{
+    TEST(Test_servicioPostal_Segunda, Test_pocoPeso)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        EnvioServicioPostal servicioPostal;
+
+        // Act - ejecute la operación
+        int actual = servicioPostal.Calcular(2, 3, 1);
+        int esperada = 0.0280;
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
+
+    TEST(Test_servicioPostal_Segunda, Test_medioPeso)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        EnvioServicioPostal servicioPostal;
+
+        // Act - ejecute la operación
+        int actual = servicioPostal.Calcular(2, 6, 1);
+        int esperada = 0.0530;
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
+
+    TEST(Test_servicioPostal_Segunda, Test_muchoPeso)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        EnvioServicioPostal servicioPostal;
+
+        // Act - ejecute la operación
+        int actual = servicioPostal.Calcular(2, 9, 1);
+        int esperada = 0.0750;
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
+}
