7895e877243ee999b00467969b9a90fe62ba7cb8
JuanAguilarTorres <Juan.aguilartorres@ucr.ac.cr>
2022-04-24 23:06:17 -0600

    Creada base de clase abstracta "envio"

---
+++ src/envio.cpp
@@ -0,0 +1,5 @@
+#include "envio.h"
+
+envio::~envio() {
+    
+}
---
+++ src/envio.h
@@ -0,0 +1,16 @@
+#ifndef ENVIO_H
+#define ENVIO_H
+
+class envio {
+
+    // Privado
+
+    protected:
+    int distancia;
+    int peso;
+
+    public:
+    virtual ~envio();
+};
+
+#endif
