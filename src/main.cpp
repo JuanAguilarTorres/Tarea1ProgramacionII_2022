31463ae5aa9e0c5b9d61a9ce816f7b84cb4c5f0b
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2022-05-01 01:51:43 -0600

    Lógica de main.ccp creada

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,30 @@
 #include <vector>
 #include <iostream>
 
-int main()
-{
+using namespace std;
 
+int main()
+{ 
+    //Ejemplo
+    vector<envio *> arregloEnvios;
+    double totalMontoEnvios = 0;
+
+    envioFedEX* primerEnvio = new envioFedEX();
+    arregloEnvios.push_back(primerEnvio);
+
+    envioServicioPostal* segundoEnvio = new envioServicioPostal();
+    arregloEnvios.push_back(segundoEnvio);
+
+    envioFedEX* cuartoEnvio = new envioFedEX();
+    arregloEnvios.push_back(cuartoEnvio);
+
+    envioServicioPostal* quintoEnvio = new envioServicioPostal();
+    arregloEnvios.push_back(quintoEnvio);
+
+    for(envio* envio: arregloEnvios)
+    {
+        totalMontoEnvios += envio->calcular();
+    }
+
+    std::cout << "El monto total de los envíos es de " << totalMontoEnvios << std::endl;
 }
